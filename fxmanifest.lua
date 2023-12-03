a8339b388546ed7de71cfe6a8860a1e1a6b7f8c1
YoshiTv-Development <142677543+YoshiTv-Development@users.noreply.github.com>
2023-12-03 20:54:38 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 author 'YoshiTv' 
 description 'User Info'
 version '1.0'
-discord 'https://discord.gg/XnYDhzWyuq'
+discord 'https://discord.gg/v73eQpGp5b'
 
 shared_script {
     '@es_extended/imports.lua',
